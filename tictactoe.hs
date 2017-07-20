e6cc996af43718ef5f6873788fbd228dc8cce94d
Jordan <jlevin38@binghamton.edu>
2017-07-19 21:08:42 -0400

    IT SORTA WORKS. entering positions is now supported

--- tictactoe.hs
+++ tictactoe.hs
@@ -4,36 +4,43 @@ import Control.Monad
 
 {-data Board = Board Char Char Char Char Char Char Char Char Char-}
 {-data Board = Board [[Char]]-}
-data Board = Board [Char] deriving (Show)
+data Board = Board [[Char]] deriving (Show)
 
 {-main = do-}
     {-let board = Board ((' ',' ',' '),(' ',' ',' '),(' ',' ',' '))-}
+    {-let board = Board [' ',' ',' ',' ',' ',' ',' ',' ',' ']-}
     {-let board = Board ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '-}
-    {-let board = Board [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]-}
     {-turn board-}
 main = do
-    let board = Board [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-    gameLoop board
+    let board = Board [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+    gameLoop board 'X'
 
-gameLoop :: Board -> IO ()
-gameLoop (Board info) = forever $ do
-    putStrLn "Enter a move (Eventually this will be coords)" 
-    num <- getLine
-    putStrLn "Enter a character (this is temporary)"
-    val <- getChar
-    let pos = (read num :: Int)
-    let b = Board info
-    printBoard $ turn b pos val
+gameLoop :: Board -> Char -> IO ()
+gameLoop (Board info) turn = do
+    {-hSetBuffering stdin NoBuffering-}
+    putStrLn "Enter x coord" 
+    x <- getLine
+    putStrLn "Enter y coord" 
+    y <- getLine
+    let next = if turn == 'X' then 'O' else 'X'
+    printBoard $ replaceRow (Board info) (read x :: Int) (read y :: Int) (turn) 
+    gameLoop (replaceRow (Board info) (read x :: Int) (read y :: Int) (turn)) next
 
+replaceRow :: Board -> Int -> Int -> Char -> Board
+replaceRow (Board info) 1 y c = Board ([replacePoint (info !! 0) y c] ++ [info !! 1] ++ [info !! 2])
+replaceRow (Board info) 2 y c = Board ([info !! 0] ++ [replacePoint (info !! 1) y c] ++ [info !! 2])
+replaceRow (Board info) 3 y c = Board ([info !! 0] ++ [info !! 1] ++ [replacePoint (info !! 2) y c])
     
+replacePoint :: [Char] -> Int -> Char -> [Char]
+replacePoint row 1 c = [c] ++ [row !! 1] ++ [row !! 2]
+replacePoint row 2 c = [row !! 0] ++ [c] ++ [row !! 2]
+replacePoint row 3 c = [row !! 0] ++ [row !! 1] ++ [c]
 
-turn :: Board -> Int -> Char -> Board
-turn (Board info) pos c = Board (take (pos-1) info ++ c:drop pos info)
 
 printBoard :: Board -> IO ()
 printBoard (Board info) = do
-    let l1 = take 3 info
-    let l2 = drop 3 $ take 6 info
-    let l3 = drop 6 info
+    let l1 = info !! 0
+    let l2 = info !! 1
+    let l3 = info !! 2
     putStr $ unlines [l1, l2, l3]
 
