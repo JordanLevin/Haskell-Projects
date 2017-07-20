4e86011e5bb245fb1b91cae5143a9efd683567a7
Jordan Levin <jlevin38@binghamton.edu>
2017-07-20 15:18:43 -0400

    bst insert works

--- BinarySearchTree.hs
+++ BinarySearchTree.hs
@@ -1,6 +1,9 @@
-data Tree a = Leaf a | Leaf a Tree a Tree a
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 
 -- Insert second value into the first tree and return a new tree
-insert :: (Ord a) => Tree -> a -> Tree
-insert (Leaf x) val = Leaf val
-insert (Tree leaf t1 t2) = if val < leaf then insert t1 val else insert t2 val
+insert :: (Ord a) => Tree a -> a -> Tree a
+insert Empty val = Node val Empty Empty
+insert (Node val t1 t2) input
+    | input < val = Node val (insert t1 input) t2
+    | input > val = Node val (insert t2 input) t1
+    | input == val = error "Cannot have duplicates in a bst"
