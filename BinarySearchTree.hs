b8d6e99d72977a8f7a1e6bfa16d177094d898a3f
Jordan <jlevin38@binghamton.edu>
2017-07-25 12:46:17 -0400

    added find min and find max to help with remove

--- BinarySearchTree.hs
+++ BinarySearchTree.hs
@@ -12,13 +12,13 @@ insert (Node val t1 t2) input
 (.+) :: (Ord a) => Tree a -> a -> Tree a
 tree .+ val = insert tree val
 
---Returns the size of a tree
+-- Returns the size of a tree
 size :: Tree a -> Int
 size Empty = 0
 size (Node _ Empty Empty) = 1
 size (Node _ t1 t2) = 1 + size t1 + size t2
 
---Returns a list containing all values traversed in order (I think)
+-- Returns a list containing all values traversed in order (I think)
 traverseBST :: Tree a -> [a]
 traverseBST Empty = []
 traverseBST (Node val t1 t2) = traverseBST t2 ++ val : traverseBST t1
@@ -30,13 +30,37 @@ find (Node val t1 t2) target
     | target == val = True
     | target > val = find t1 target 
     | target < val = find t2 target 
+
+-- Finds min val in a tree
+findMin :: (Ord a) => Tree a -> a
+findMin Empty = error "Empty tree has no min value"
+findMin x = head $ traverseBST x
+
+-- Finds max val in a tree
+findMax :: (Ord a) => Tree a -> a
+findMax Empty = error "Empty tree has no max value"
+findMax x = last $ traverseBST x
+
+-- Makes a tree with some ints for testing purposes
+makeTestTree :: Tree Int
+makeTestTree = do
+    let a = insert Empty 10
+    let b = a .+ 6
+    let c = b .+ 2
+    let d = c .+ 17
+    let e = d .+ 12
+    e
 --works up to here
 
 --Find a certain value and return a new BST without that value in it
 --Remove handles root, if that doesn't happen it passed to remove helper
 remove :: (Ord a) => Tree a -> a -> Tree a
-remove Empty _ = Empty
-remove (Node val t1 t2) target
-    | val == target = 
+remove Empty _ = Empty --Tree is empty
+remove (Node val Empty Empty) target--Root with 2 null children
+    | val == target = Empty
+    | otherwise = Node val Empty Empty
+remove (Node val t1 Empty) target
+    {-| ????????????-}
+-- need a way to combine trees
 
-removeHelper :: (Ord a) => Tree a -> a -> Tree a
+{-removeHelper :: (Ord a) => Tree a -> a -> Tree a-}
