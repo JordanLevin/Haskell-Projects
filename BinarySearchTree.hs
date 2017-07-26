ac674d1e66831eb1bce9fb2aa993c74f50c92162
Jordan Levin <jlevin38@binghamton.edu>
2017-07-26 11:15:28 -0400

    bst remove completed I think, might still miss some edge cases

--- BinarySearchTree.hs
+++ BinarySearchTree.hs
@@ -4,8 +4,8 @@ data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 insert :: (Ord a) => Tree a -> a -> Tree a
 insert Empty val = Node val Empty Empty
 insert (Node val t1 t2) input
-    | input < val = Node val t1 (insert t2 input) 
-    | input > val = Node val (insert t1 input) t2
+    | input > val = Node val t1 (insert t2 input) 
+    | input < val = Node val (insert t1 input) t2
     | input == val = error "Cannot have duplicates in a bst"
 
 --Infix synonym for insert for easier typing
@@ -21,15 +21,15 @@ size (Node _ t1 t2) = 1 + size t1 + size t2
 -- Returns a list containing all values traversed in order (I think)
 traverseBST :: Tree a -> [a]
 traverseBST Empty = []
-traverseBST (Node val t1 t2) = traverseBST t2 ++ val : traverseBST t1
+traverseBST (Node val t1 t2) = traverseBST t1 ++ val : traverseBST t2
 
 --Returns a boolean based on if a value is in the tree
 find :: (Ord a) => Tree a -> a -> Bool
 find Empty _ = False
 find (Node val t1 t2) target
     | target == val = True
-    | target > val = find t1 target 
-    | target < val = find t2 target 
+    | target > val = find t2 target 
+    | target < val = find t1 target 
 
 -- Finds min val in a tree
 findMin :: (Ord a) => Tree a -> a
@@ -49,18 +49,28 @@ makeTestTree = do
     let c = b .+ 2
     let d = c .+ 17
     let e = d .+ 12
-    e
---works up to here
+    let f = e .+ 8
+    f
 
---Find a certain value and return a new BST without that value in it
---Remove handles root, if that doesn't happen it passed to remove helper
+-- Find a certain value and return a new BST without that value in it
 remove :: (Ord a) => Tree a -> a -> Tree a
-remove Empty _ = Empty --Tree is empty
-remove (Node val Empty Empty) target--Root with 2 null children
-    | val == target = Empty
+-- Tree is empty
+remove Empty _ = Empty 
+-- Tree is a leaf node
+remove (Node val Empty Empty) target
+    | target == val = Empty
     | otherwise = Node val Empty Empty
+-- Tree only has 1 child branch
 remove (Node val t1 Empty) target
-    {-| ????????????-}
--- need a way to combine trees
-
-{-removeHelper :: (Ord a) => Tree a -> a -> Tree a-}
+    | target == val = t1
+    | otherwise = Node val (remove t1 target) Empty
+-- Tree only has 1 child branch
+remove (Node val Empty t2) target
+    | target == val = t2
+    | otherwise = Node val Empty $ remove t2 target
+-- Tree has two children
+remove (Node val t1 t2) target
+    | target == val = Node (t1Max) (remove t1 t1Max) t2
+    | target < val = Node val (remove t1 target) t2
+    | target > val = Node val t1 (remove t2 target)
+    where t1Max = findMax t1
