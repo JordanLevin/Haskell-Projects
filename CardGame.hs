493fc7794caff077f1c7e29845958a826095babb
Jordan <jlevin38@binghamton.edu>
2017-07-29 21:22:01 -0400

    started working on a deck of cards thing

---
+++ CardGame.hs
@@ -0,0 +1,27 @@
+import System.Random
+    
+data Suit = Spade | Heart | Club | Diamond deriving (Show)
+data Card = Card Suit Int deriving (Show)
+data Deck = Deck [Card] deriving (Show)
+
+{-draw :: Deck -> Deck-}
+
+makeDeck :: Deck
+makeDeck = Deck $ makeDeckHelper 13 
+
+makeDeckHelper :: Int -> [Card]
+makeDeckHelper 1 = 
+    [(Card Spade 1), (Card Heart 1) , (Card Club 1) , (Card Diamond 1)]
+makeDeckHelper x = 
+    [(Card Spade x), (Card Heart x) , (Card Club x) , (Card Diamond x)] ++ (makeDeckHelper $ x-1)
+
+drawPos :: Deck -> Int -> (Deck, Card)
+drawPos (Deck cards) pos
+    | pos >= length cards = error "Invalid position in deck"
+    | otherwise = (Deck $ (take pos cards) ++ (drop (pos+1) cards), cards !! pos)
+
+drawTop :: Deck -> (Deck, Card)
+drawTop (Deck cards) = drawPos (Deck cards) $ length cards - 1
+
+
+{-shuffleDeck :: Deck -> Deck-}
